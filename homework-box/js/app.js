5281df545f27ae33de048af1b88c37b597901b18
Gulu <gulusv19999@gmail.com>
2021-12-26 22:20:44 +0400

    Box navbar task

---
+++ homework-box/js/app.js
@@ -0,0 +1,10 @@
+document.addEventListener("click",btn)
+function btn() {
+    var btn= document.querySelector('.my-animate')
+    btn.classList.toggle('get')
+    document.body.classList.toggle("color");
+
+
+
+    
+}
